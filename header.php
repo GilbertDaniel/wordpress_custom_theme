0771a6954e3fec76c713f8d9d4238559104357a9
GilbertDaniel <daniel.gilbert247@gmail.com>
2022-10-14 20:58:38 +0630

    Implementing Dynamic Menus

--- header.php
+++ header.php
@@ -32,8 +32,20 @@
 
 			<div id="navigation" class="collapse navbar-collapse flex-column" >
 				<img class="mb-3 mx-auto logo" src="images/logo.png" alt="logo" >			
+				<?php
+				wp_nav_menu(
+					array(
+						'menu' => 'main_menu',
+						'theme_location' => 'primary',
+						'container' => 'ul',
+						'menu_class' => 'navbar-nav flex-column text-sm-center text-md-left',
+					)
+				); 
+
 				
-				<ul class="navbar-nav flex-column text-sm-center text-md-left">
+
+				?>
+				<!-- <ul class="navbar-nav flex-column text-sm-center text-md-left">
 					<li class="nav-item active">
 					    <a class="nav-link" href="index.html"><i class="fas fa-home fa-fw mr-2"></i>Blog Home <span class="sr-only">(current)</span></a>
 					</li>
@@ -49,7 +61,7 @@
 					<li class="nav-item">
 					    <a class="nav-link btn btn-primary" href="contact.html"><i class="fas fa-envelope fa-fw mr-2"></i>Contact Us</a>
 					</li>
-				</ul>
+				</ul> -->
 				<hr>
 				<ul class="social-list list-inline py-3 mx-auto">
 					<li class="list-inline-item"><a href="#"><i class="fab fa-twitter fa-fw"></i></a></li>
