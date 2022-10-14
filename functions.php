51f60fb192d93a9e1c0b5aafbba885865abf717e
GilbertDaniel <daniel.gilbert247@gmail.com>
2022-10-14 22:59:38 +0630

    Add Feature Image Thumbnail Support

--- functions.php
+++ functions.php
@@ -3,6 +3,7 @@ function mycustomtheme_theme_support(){
     // Add dynamic title tag support
     add_theme_support('title-tag');
     add_theme_support('custom-logo');
+    add_theme_support('post-thumbnails');
 }
 
 add_action('after_setup_theme', 'mycustomtheme_theme_support');
